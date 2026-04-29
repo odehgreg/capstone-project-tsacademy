234f0dff343ca721efaab5eca4beaec8b888282c
odehgreg <jamesodeh80@gmail.com>
2026-04-29 18:34:56 +0100

    Replace assignment README with project-specific capstone README

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "novara-terraform-state-odeh-james-euw1"
+    bucket         = "odehgreg-tfstate-euw1-1777482285"
     key            = "capstone-eu-west-1/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks-euw1"
+    encrypt        = true
   }
 }
